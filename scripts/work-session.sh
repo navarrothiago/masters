a37f525feeb174718ce21add9496feea96add68d
Thiago Arruda Navarro do Amaral <navarro.ime@gmail.com>
2020-04-17 09:35:55 -0300

    Updates imgs and scripts

--- scripts/work-session.sh
+++ scripts/work-session.sh
@@ -18,6 +18,7 @@ sessionStart(){
     # Initialize windowns and panes
     tmux kill-session -t $TMUX_SESSION 2>/dev/null 
     tmux -2 new-session -d -s $TMUX_SESSION 
+    tmux new-window -d -t $TMUX_SESSION -n epc
     tmux new-window -d -t $TMUX_SESSION -n jupyter 
     tmux new-window -d -t $TMUX_SESSION -n tat 
     tmux split-window -t $TMUX_SESSION:0.0 -v 
@@ -25,8 +26,8 @@ sessionStart(){
     tmux send-keys -t $TMUX_SESSION:0.0 "virsh --connect qemu:///system start $HOSTNAME_EPC; sleep 10; ssh -X -o ConnectTimeout=10 epc@$IP_EPC -t " C-m 
     #tmux send-keys -t $TMUX_SESSION:0.1 "virsh --connect qemu:///system start enodeb; sleep 10; ssh -o ConnectTimeout=10 enodeb@192.168.11.15 -t " C-m 
     tmux send-keys -t $TMUX_SESSION:0.1 "ssh -X $HOSTNAME_ENODEB@$IP_ENODEB" C-m 
-    tmux send-keys -t $TMUX_SESSION:1.0 "/usr/local/etc/anaconda3/bin/jupyter notebook --notebook-dir=/work/unicamp/masters --no-browser" C-m 
-    tmux send-keys -t $TMUX_SESSION:2.0 "tat /work/unicamp/masters/scripts/logs/192.168.15.14-mme-link.log /Filters:/work/unicamp/masters/tat-filters/oai-epc-filter.tat" C-m 
+    tmux send-keys -t $TMUX_SESSION:2.0 "/usr/local/etc/anaconda3/bin/jupyter notebook --notebook-dir=/work/unicamp/masters --no-browser" C-m 
+    tmux send-keys -t $TMUX_SESSION:3.0 "tat /work/unicamp/masters/scripts/logs/192.168.15.14-mme-link.log /Filters:/work/unicamp/masters/tat-filters/oai-epc-filter.tat" C-m 
 }
 
 sessionAttach(){
